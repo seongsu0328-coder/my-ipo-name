02196b1e54d59c39b197b9742ddc612455ddff2a
seongsu0328-coder <seongsu0328@naver.com>
2026-02-17 10:52:41 +0900

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -1682,7 +1682,7 @@ elif st.session_state.page == 'setup':
                 font-size: 1rem;
                 margin-bottom: 10px;
             ">
-                환영합니다, <b>{user_id}</b>님! 활동닉네임과 노출범위를 확인해주세요. <인증회원은 글쓰기와 투표참여가 가능합니다.
+                환영합니다, <b>{user_id}</b>님! 활동닉네임과 노출범위를 확인해주세요. 인증회원은 글쓰기와 투표참여가 가능합니다.
             </div>
         """, unsafe_allow_html=True)
         
